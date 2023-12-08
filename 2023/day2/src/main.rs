c68b077b214ce4f60c234c756b2b70969eef5efd
Benjamin Schoenberger <bschoe322@gmail.com>
2023-12-07 22:27:43 -0500

    2023 d2 rust impl

--- 2023/day2/src/main.rs
+++ 2023/day2/src/main.rs
@@ -2,18 +2,32 @@ use std::collections::HashMap;
 
 fn main() {
     let f = std::fs::read_to_string("input.txt").unwrap();
-    let lines = f.lines().map(|e| e.to_string()).collect::<Vec<_>>();
+    let lines = f.lines().collect::<Vec<_>>();
     solve_part_one(&lines);
+    solve_part_two(&lines);
 }
 
-struct Game {
-    id: i32,
-    bags: Vec<HashMap<String, i32>>,
+struct Game<'a> {
+    id: usize,
+    bags: Vec<HashMap<&'a str, usize>>,
 }
 
-impl Game {
-    fn new(line: &String, ignore_max: bool) -> Self {
-        let mut bags = Vec::<HashMap<String, i32>>::new();
+const RED_MAX: usize = 12;
+const GREEN_MAX: usize = 13;
+const BLUE_MAX: usize = 14;
+
+fn is_at_max(name: &str, amount: usize) -> bool {
+    match name {
+        "red" => amount > RED_MAX,
+        "green" => amount > GREEN_MAX,
+        "blue" => amount > BLUE_MAX,
+        _ => unreachable!(),
+    }
+}
+
+impl<'a> Game<'a> {
+    fn new(line: &'a str, ignore_max: bool) -> Option<Self> {
+        let mut all_bags = Vec::<HashMap<&str, usize>>::new();
 
         let (id_part, other) = line.split_once(": ").unwrap();
 
@@ -22,26 +36,64 @@ impl Game {
             .split_ascii_whitespace()
             .nth(1)
             .unwrap()
-            .parse::<i32>()
+            .parse::<usize>()
             .unwrap();
 
         let bag_groups = other.split("; ");
 
         for group in bag_groups {
-            let mut bags = HashMap::<String, i32>::new();
+            let mut bags = HashMap::<&str, usize>::new();
 
             for bag in group.split(", ") {
-                let (rawCount, name) = bag.split_once(' ').unwrap();
-                let count = rawCount.parse::<i32>().unwrap();
+                let (raw_count, name) = bag.split_once(' ').unwrap();
+                let count = raw_count.parse::<usize>().unwrap();
+
+                if !ignore_max && is_at_max(name, count) {
+                    return None;
+                }
+
+                bags.insert(name, count);
             }
+
+            all_bags.push(bags);
         }
 
-        return Self { id: game_id, bags };
+        return Some(Self {
+            id: game_id,
+            bags: all_bags,
+        });
     }
 }
 
-fn solve_part_one(lines: &Vec<String>) {
+fn solve_part_one(lines: &[&str]) {
+    let mut sum: usize = 0;
+
     for l in lines {
-        let _ = Game::new(l, false);
+        let Some(game) = Game::new(l, false) else {
+            continue;
+        };
+        sum += game.id;
     }
+
+    println!("Part 1: {sum}");
+}
+
+fn solve_part_two(lines: &[&str]) {
+    let mut sum: usize = 0;
+
+    for l in lines {
+        let game = Game::new(l, true).unwrap();
+
+        let mut mins: HashMap<&str, usize> = HashMap::new();
+
+        for group in game.bags {
+            for (key, value) in group.iter() {
+                mins.entry(key).and_modify(|v| *v = *value.max(v)).or_insert(*value);
+            }
+        }
+
+        sum += mins.values().product::<usize>();
+    }
+
+    println!("Part 2: {sum}");
 }
